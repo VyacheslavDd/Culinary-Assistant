4b77199ade4c6eb35310d08adade7a09d3456509
VyacheslavDd <Sonic.x.exe@yandex.ru>
2025-03-19 18:57:23 +0500

    Backend initialized + CI .net

---
+++ Backend/Culinary-Assistant-Main/Culinary-Assistant-Main.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Culinary_Assistant_Main.Tests
+{
+	public class Tests
+	{
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+		[Test]
+		public void Test1()
+		{
+			Assert.Pass();
+		}
+	}
+}
